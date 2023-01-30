d68f7565e725fc48c8ae25e5b3df2156b5c60244
Adrian Jansen Quindeler <122066256+Adrian-JQuindeler@users.noreply.github.com>
2023-01-30 20:53:26 -0300

    'Olá, mundo!' - Português

--- A.py
+++ A.py
@@ -7,10 +7,11 @@ texto = input("escreva 'Olá, mundo!'\n")
 while True:
     if texto == 'Olá, mundo!':
         escrevaEntreLinhas(texto)
-        print("\nParabéns, escreveu certo\n")
+        print("\nParabéns, escreveu certo!!!\n")
         break
     else:
         print("Não era bem isso que você deveria escrever...\n")
 print("________________")
 print("Volte sempre!")
 print("________________")
+'Olá, mundo!' - Português
