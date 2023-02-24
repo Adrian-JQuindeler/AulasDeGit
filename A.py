8dee68a424faeba25d2bc29b5189812530c2ac62
Adrian Jansen Quindeler <122066256+Adrian-JQuindeler@users.noreply.github.com>
2023-02-24 16:23:25 -0300

    'Olá, mundo!' - Português

--- A.py
+++ A.py
@@ -6,8 +6,12 @@ def escrevaEntreLinhas(texto):
     print(f" \033[1m{texto}\033[m ")
     print("\033[9m_\033[m" * tamanho)
 
-
 print("Por favor faça o que lhe for solicitado:")
+print("Este programa apenas ira conferir se voce tem a capacidade de reescrever um texto que será exibido na tela")
+
+print("Por favor, copie exatamente da forma que está escrita, incluindo acentos, caracteres especiais, e etc...")
+
+print("O uso incorreto de maiúsculas e minusculas é permitido, porem sera considerado para o resultado final")
 while True:
     frase = 'Olá, mundo!'
     texto = input("escreva 'Olá, mundo!'\n").lower().strip()
